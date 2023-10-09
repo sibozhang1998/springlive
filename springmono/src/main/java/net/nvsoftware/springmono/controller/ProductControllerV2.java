0339eaf81233222a572559d4f6d5acb7f52b9f86
Sibo Zhang <sibozhang1998@gmail.com>
2023-10-08 22:35:22 -0400

    add jpa and h2db, config h2db, intro entity and repository, upgrade to v2 api, now use h2db instead of list

--- springmono/src/main/java/net/nvsoftware/springmono/controller/ProductControllerV2.java
+++ springmono/src/main/java/net/nvsoftware/springmono/controller/ProductControllerV2.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/product")
-public class ProductController {
-
-    @Qualifier("productServiceImpl")
+@RequestMapping("/v2/product")
+public class ProductControllerV2 {
+    
+    @Qualifier("productServiceImplV2")
     @Autowired
     private ProductService productService;
 
