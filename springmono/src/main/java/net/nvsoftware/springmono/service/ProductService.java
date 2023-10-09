c1beb9a8f6b5bb9abe8877cc742271c331959c15
Sibo Zhang <sibozhang1998@gmail.com>
2023-10-08 21:00:39 -0400

    add product crud, getall, getbyid, deletebyid, save

--- springmono/src/main/java/net/nvsoftware/springmono/service/ProductService.java
+++ springmono/src/main/java/net/nvsoftware/springmono/service/ProductService.java
@@ -3,6 +3,11 @@ package net.nvsoftware.springmono.service;
 import net.nvsoftware.springmono.model.Product;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 public interface ProductService {
     Product save(Product product);
+    List<Product> getAll();
+    Product getById(String id);
+    String deleteById(String id);
 }
