9eb03620162b31934625c342f227b102a27064a2
Sibo Zhang <sibozhang1998@gmail.com>
2023-10-21 22:15:07 -0400

    for ProductService CRUD, addProduct

--- ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductService.java
+++ ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductService.java
@@ -1,4 +1,7 @@
 package net.nvsoftware.ProductService.service;
 
+import net.nvsoftware.ProductService.model.ProductRequest;
+
 public interface ProductService {
+    Long addProduct(ProductRequest productRequest);
 }
