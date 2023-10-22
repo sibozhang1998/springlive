eeec8d56d7acb2ea28af773edaf7ab2b9bedbe76
Sibo Zhang <sibozhang1998@gmail.com>
2023-10-22 01:42:05 -0400

    add log to orderService and controller

--- OrderService/src/main/java/net/nvsoftware/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/controller/OrderController.java
@@ -1,5 +1,6 @@
 package net.nvsoftware.OrderService.controller;
 
+import lombok.extern.log4j.Log4j2;
 import net.nvsoftware.OrderService.model.OrderRequest;
 import net.nvsoftware.OrderService.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
+@Log4j2
 public class OrderController {
     @Autowired
     private OrderService orderService;
 
     @PostMapping("/placeOrder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Start: OrderService controller placeOrder");
         long orderId = orderService.placeOrder(orderRequest);
+        log.info("End: OrderService controller placeOrder");
         return new ResponseEntity<>(orderId, HttpStatus.OK);
     }
 
--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package net.nvsoftware.OrderService.service;
 
+import lombok.extern.log4j.Log4j2;
 import net.nvsoftware.OrderService.model.OrderRequest;
 import net.nvsoftware.OrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
@@ -13,11 +15,13 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {
+        log.info("Start: OrderService placeOrder");
         // use OrderService create OrderEntity with orderStatus CREATED, ORM Jpa save to DB
 
         // call ProductService to check product quantity, if OK, reduce it, else throw not enough exception
 
         // call PaymentService to charge, if Success, mark order PAID, else CANCELED
+        log.info("End: OrderService placeOrder");
         return 0;
     }
 }
