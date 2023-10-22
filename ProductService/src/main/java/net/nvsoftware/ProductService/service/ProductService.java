ae7524fac9de93100b9aeb9eb38edfefdb8c0df3
Sibo Zhang <sibozhang1998@gmail.com>
2023-10-22 13:16:17 -0400

    productService reduceQuantity api

--- ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductService.java
+++ ProductService/src/main/java/net/nvsoftware/ProductService/service/ProductService.java
@@ -6,4 +6,5 @@ import net.nvsoftware.ProductService.model.ProductResponse;
 public interface ProductService {
     Long addProduct(ProductRequest productRequest);
     ProductResponse getById(long id);
+    void reduceQuantity(long id, long quantity);
 }
