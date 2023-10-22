5428b093a1ec7536a568704eafc93a4ee4e47dac
Sibo Zhang <sibozhang1998@gmail.com>
2023-10-22 16:13:10 -0400

    for OrderService feignCall PaymentService doPayment api

--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
@@ -1,9 +1,11 @@
 package net.nvsoftware.OrderService.service;
 
 import lombok.extern.log4j.Log4j2;
+import net.nvsoftware.OrderService.client.PaymentServiceFeignClient;
 import net.nvsoftware.OrderService.client.ProductServiceFeignClient;
 import net.nvsoftware.OrderService.entity.OrderEntity;
 import net.nvsoftware.OrderService.model.OrderRequest;
+import net.nvsoftware.OrderService.model.PaymentRequest;
 import net.nvsoftware.OrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,8 @@ public class OrderServiceImpl implements OrderService {
     private OrderRepository orderRepository;
     @Autowired
     private ProductServiceFeignClient productServiceFeignClient;
+    @Autowired
+    private PaymentServiceFeignClient paymentServiceFeignClient;
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {
@@ -38,7 +42,25 @@ public class OrderServiceImpl implements OrderService {
         log.info("Process: OrderService placeOrder FeignCall ProductService reduceQuantity");
 
         // call PaymentService to charge, if Success, mark order PAID, else CANCELED
-        log.info("End: OrderService placeOrder");
+        PaymentRequest paymentRequest = PaymentRequest.builder()
+                .orderId(orderEntity.getId())
+                .paymentMode(orderRequest.getPaymentMode())
+                .totalAmount(orderRequest.getTotalAmount())
+                .build();
+        String orderStatus = null;
+
+        try {
+            paymentServiceFeignClient.doPayment(paymentRequest);
+            orderStatus = "PAID";
+            log.info("Process: Service placeOrder FeignCall PaymentService doPayment PAID");
+        } catch (Exception e) {
+            orderStatus = "PAYMENT_FAILED";
+            log.info("Process: Service placeOrder FeignCall PaymentService doPayment FAILED");
+        }
+
+        orderEntity.setOrderStatus(orderStatus);
+        orderRepository.save(orderEntity);
+        log.info("End: OrderService placeOrder Done with orderId: " + orderEntity.getId());
         return orderEntity.getId();
     }
 }
